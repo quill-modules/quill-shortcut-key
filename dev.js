254312a64304614b0fdb396c7e4a5edb42a26cb7
zzxming <74341337+zzxming@users.noreply.github.com>
2025-01-07 06:31:21 +0000

    Deploying to gh-pages from @ zzxming/quill-short-format@b116280e0de2007eb4a289de63aebf61ffaca019 🚀

---
+++ dev.js
@@ -0,0 +1,2 @@
+!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?t(exports):"function"==typeof define&&define.amd?define(["exports"],t):t((e="undefined"!=typeof globalThis?globalThis:e||self).bundle={})}(this,(function(e){"use strict";class t{quill;constructor(e){this.quill=e}}e.QuillQuickInsert=t,e.default=t,Object.defineProperty(e,"__esModule",{value:!0})}));
+//# sourceMappingURL=dev.js.map
