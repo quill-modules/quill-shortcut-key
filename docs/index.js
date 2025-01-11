d756b62fd0fa6877b93ee914230d2f1d9d777460
zzxming <zzxming77@163.com>
2025-01-12 05:16:32 +0800

    dcos: add toolbar tip

--- docs/index.js
+++ docs/index.js
@@ -6,10 +6,12 @@ const {
   // generateTableUpShortKeyMenu,
 } = window.QuillShortcutKey;
 // const { default: TableUp, defaultCustomSelect, createSelectBox, TableAlign, TableMenuContextmenu, TableResizeBox, TableResizeScale, TableSelection, TableVirtualScrollbar } = window.TableUp;
+const { default: QuillToolbarTip } = window.QuillToolbarTip;
 
 Quill.register({
   [`modules/shortcut-key`]: QuillShortcutKey,
   // [`modules/${TableUp.moduleName}`]: TableUp,
+  [`modules/${QuillToolbarTip.moduleName}`]: QuillToolbarTip,
 }, true);
 
 const toolbarConfig = [
@@ -23,11 +25,12 @@ const toolbarConfig = [
   [{ direction: 'rtl' }],
   [{ size: ['small', false, 'large', 'huge'] }],
   [{ header: [1, 2, 3, 4, 5, 6, false] }, { header: 1 }, { header: 2 }, { header: 3 }, { header: 4 }, { header: 5 }, { header: 6 }],
-  [{ color: [] }, { background: [] }],
-  [{ font: [] }, { align: [] }],
+  [{ font: [] }, { color: [] }, { background: [] }],
+  [{ align: '' }, { align: 'center' }, { align: 'right' }, { align: 'justify' }],
   // [{ [TableUp.toolName]: [] }],
 ];
 
+console.log(defaultShortKey);
 // const { tableUpConfig, tableUpKeyboardControl } = generateTableUpShortKeyMenu(createSelectBox);
 const quill1 = new Quill('#editor1', {
   // debug: 'info',
@@ -37,6 +40,49 @@ const quill1 = new Quill('#editor1', {
     'keyboard': {
       bindings: {
         ...defaultShortKey,
+      },
+    },
+    [QuillToolbarTip.moduleName]: {
+      defaultTooltipOptions: {
+        tipHoverable: false,
+      },
+      tipTextMap: {
+        'background': 'Background',
+        'blockquote': 'Blockquote',
+        'bold': 'Bold\nctrl+b',
+        'clean': 'Clean',
+        'code': 'Code\nctrl+e',
+        'color': 'Color',
+        'formula': 'Formula',
+        'italic': 'Italic\nctrl+i',
+        'image': 'Image',
+        'strike': 'Strike\nctrl+d',
+        'underline': 'Underline\nctrl+u',
+        'video': 'Video',
+        'link': 'Link',
+        'list:ordered': 'Ordered List',
+        'list:bullet': 'Unordered List',
+        'list:check': 'Todo List',
+        'align:left': 'Left aligned\nalt+l',
+        'align:center': 'Center aligned\nalt+c',
+        'align:right': 'Right aligned\nalt+r',
+        'align:justify': 'Justify aligned\nalt+j',
+        'script:super': 'Superscript\nctrl+,',
+        'script:sub': 'Subscript\nctrl+.',
+        'indent:-1': 'Minus Indent\nctrl+[',
+        'indent:+1': 'Add Indent\nctrl+]',
+        'text': 'Text',
+        'header:1': 'Heading 1',
+        'header:2': 'Heading 2',
+        'header:3': 'Heading 3',
+        'header:4': 'Heading 4',
+        'header:5': 'Heading 5',
+        'header:6': 'Heading 6',
+        'direction': {
+          onShow(target) {
+            return target.classList.contains('ql-active') ? 'Text Direction Right To Left\nctrl+r' : 'Text Direction Left To Right\nctrl+l';
+          },
+        },
       },
     },
     'shortcut-key': {
